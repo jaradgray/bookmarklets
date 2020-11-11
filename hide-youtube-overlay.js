90391ef66159a59ac0b03985e85624152b830ccb
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-10 21:33:58 -0600

    prettify source

--- hide-youtube-overlay.js
+++ hide-youtube-overlay.js
@@ -1,14 +1,18 @@
-// javascript: (function() {
-//     var goaway = ".ytp-chrome-top, .ytp-chrome-bottom, .ytp-gradient-top, .ytp-gradient-bottom {display:none;}";
-//     if("\v" == "v") {
-//         document.createStyleSheet().cssText = goaway
-//     }
-//     else {
-//         var tag = document.createElement("style");
-//         tag.type = "text/css";
-//         document.getElementsByTagName("head")[0].appendChild(tag);
-//         tag[(typeof document.body.style.WebkitAppearance == "string") ? "innerText" : "innerHTML"] = goaway
-//     }
-// })();
+// Based on this Stack Exchange answer: https://superuser.com/a/1052944
 
+// Beautified
+javascript: (function() {
+    var goaway = ".ytp-chrome-top, .ytp-chrome-bottom, .ytp-gradient-top, .ytp-gradient-bottom {display:none;}";
+    if("\v" == "v") {
+        document.createStyleSheet().cssText = goaway
+    }
+    else {
+        var tag = document.createElement("style");
+        tag.type = "text/css";
+        document.getElementsByTagName("head")[0].appendChild(tag);
+        tag[(typeof document.body.style.WebkitAppearance == "string") ? "innerText" : "innerHTML"] = goaway
+    }
+})();
+
+// Minified
 javascript:(function(){var goaway=".ytp-chrome-top,.ytp-chrome-bottom,.ytp-gradient-top,.ytp-gradient-bottom{display:none;}";if("\v"=="v"){document.createStyleSheet().cssText=goaway}else{var tag=document.createElement("style");tag.type="text/css";document.getElementsByTagName("head")[0].appendChild(tag);tag[(typeof document.body.style.WebkitAppearance=="string")?"innerText":"innerHTML"]=goaway}})();
